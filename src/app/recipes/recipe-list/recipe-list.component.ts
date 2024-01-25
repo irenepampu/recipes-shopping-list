c7d01b277576bdd23fe40bbfaeda63aca7457984
Irene Pampu <irene.pampu22@gmail.com>
2024-01-25 17:51:20 +0200

    updated: working nav bar, working recipes list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,15 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'First recipe',
+      'This is a description',
+      'https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665_1280.jpg'
+    ),
+    new Recipe(
+      'Second recipe',
       'This is a description',
       'https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665_1280.jpg'
     ),
@@ -18,4 +24,8 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
